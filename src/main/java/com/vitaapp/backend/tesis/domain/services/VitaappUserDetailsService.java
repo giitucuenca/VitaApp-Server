c6cbee9b3e39ec21a0284fcdf02700065ae73dbb
luis <eugenio2305@hotmail.com>
2023-01-30 19:43:19 -0500

    tests

--- src/main/java/com/vitaapp/backend/tesis/domain/services/VitaappUserDetailsService.java
+++ src/main/java/com/vitaapp/backend/tesis/domain/services/VitaappUserDetailsService.java
@@ -27,7 +27,6 @@ public class VitaappUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        System.out.println(s);
         String user = s;
         if (s.contains("carer-")) {
             user = s.substring(6);
@@ -41,8 +40,11 @@ public class VitaappUserDetailsService implements UserDetailsService {
             return new ElderlyDetails(elderly);
         } else if (s.contains("admin-")) {
             user = s.substring(6);
+            System.out.println(user);
             Admin admin = adminRepository.getByEmail(user);
+            System.out.println(admin.getEmail());
             admin.setEmail(s);
+            System.out.println(s);
             return new AdminDetails(admin);
         } else {
             return null;
